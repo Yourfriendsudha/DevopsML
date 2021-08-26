2ce43b24452f0876aeb8361bcfd4074df4bdf135
Yourfriendsudha <chandira93@gmail.com>
2021-08-26 22:59:20 +0530

    kube script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,18 +8,16 @@ dockerpath=docsudhachand/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-r=$(kubectl get pod api 2> /dev/null;echo $?)
-
-if [ "_$r" == "_1" ] ; then
     kubectl run api \
     --image=$dockerpath \
     --image-pull-policy="Always" \
     --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
-fi
+
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
